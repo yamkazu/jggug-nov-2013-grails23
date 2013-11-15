2d5e8103f6bad4cdcef316e2bfeb701c92b0eb07
Kazuki YAMAMOTO <yam.kazuki@gmail.com>
2013-11-15 17:18:09 +0900

    やってみよう: InstanceMethodBasedMarshallerを使ってレンダリングをカスタマイズする

--- demo/grails-app/conf/spring/resources.groovy
+++ demo/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,9 @@
-// Place your Spring DSL code here
+import grails.converters.JSON
+import org.codehaus.groovy.grails.web.converters.configuration.ObjectMarshallerRegisterer
+import org.codehaus.groovy.grails.web.converters.marshaller.json.InstanceMethodBasedMarshaller
 beans = {
+    instanceMethodBasedMarshallerRegister(ObjectMarshallerRegisterer) {
+        marshaller = new InstanceMethodBasedMarshaller()
+        converterClass = JSON
+    }
 }
--- demo/grails-app/domain/demo/Tweet.groovy
+++ demo/grails-app/domain/demo/Tweet.groovy
@@ -1,6 +1,19 @@
 package demo
 
+import grails.converters.JSON
+
 class Tweet {
     String text
     User user
+
+    def toJSON(JSON converter) {
+        converter.build {
+            id id
+            text text
+            username user.username
+        }
+
+        // MapでもOK
+        // [id: id, text: text, username: user.username]
+    }
 }
